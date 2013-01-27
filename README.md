8ad9abe578e15c6f946f02aa699488fea29e53c4
David Echols <dhechols@gmail.com>
2013-01-26 23:02:23 -0800

    Dynamic OS detection. Compare logic. Refactoring.
    These changes probably introduced some (very very minor) performance
    issues. If you have millions of messages to parse, the next update
    should fix your issues. This shouldn't cause issues for most people.
    
    Also looking to add JSON support soon for a more lightweight way of
    transmitting data.

--- README.md
+++ README.md
@@ -5,28 +5,35 @@ A python script to dump texts from iMessage from an iPhone backup to a csv file.
 
 Originally based on Jehiah Czebotar's script to dump iMessages to a csv file for dashboard parsing.
 
-This script now runs on Python 3.3 and fixes several bugs.
-
-New features:
-
-- Python 3.x support
-- Date time stamp!
-- Better unicode handling
-- Python 3.x style string format.
+This script now runs on Python 3 and fixes several bugs.
 
 See the iphonewiki for more information on how this script works. http://theiphonewiki.com/wiki/IMessage#References
 
 TODO:
 
-- Refactor to patterns.
+- When privacy is enabled, write a different file. Currently writing with and without privacy will cause duplicate items.
+- Proper error handling for Madrid flags 32773, 77825, 102405.
+- Unit tests with Nose.
+- Performance improvements.
+- Output choice. Need to add support for JSON.
+- Proper license file
+
+
+List of done:
+
 - Dynamically determine OS (Mac/Windows) and use default location.
+- Refactor to patterns.
 - Create dictionaries explicitly instead of implicitly.
 - Use argparse instead of optparse.
 - PEP8 compliance.
-- Proper error handling for Madrid flags 32773, 77825, 102405.
 - After refactor, encoding should be better handled so encoding hack shouldn't be required.
-- Unit tests with Nose.
+- Python 3 support
+- Date time stamp! (Replaced by compare logic.)
+- Better unicode handling
+- Python 3.x style string format.
 
-Future TODO:
 
+Backlog:
+
+- Proper installation (via pip)
 - Add feature to dump parsed data?
--- iphone_messages_dump.py
+++ iphone_messages_dump.py
@@ -12,9 +12,8 @@ import argparse
 import glob
 import os
 import sqlite3
-import datetime
-import time
 import csv
+import sys
 from collections import OrderedDict
 
 """
@@ -32,10 +31,15 @@ NULL: not an iMessage
 """
 MADRID_OFFSET = 978307200
 MADRID_FLAGS_SENT = [36869, 45061]
-DEFAULT_BACKUP_LOCATION_MAC = "~/Library/Application Support/MobileSync/Backup/*/3d0d7e5fb2ce288813306e4d4636395e047a3d28"
-DEFAULT_BACKUP_LOCATION_WIN = "C:\\Users\\David\\AppData\\Roaming\\Apple Computer\\MobileSync\\Backup\\*\\3d0d7e5fb2ce288813306e4d4636395e047a3d28"
-# Command line args will get parsed into this:
-args = None
+
+
+def backup_location(platform):
+    mac_names = "darwin", "osx", "mac"
+    windows_names = "win32"
+    if platform in mac_names:
+        return "~/Library/Application Support/MobileSync/Backup/*/3d0d7e5fb2ce288813306e4d4636395e047a3d28"
+    elif platform in windows_names:
+        return "C:\\Users\\David\\AppData\\Roaming\\Apple Computer\\MobileSync\\Backup\\*\\3d0d7e5fb2ce288813306e4d4636395e047a3d28"
 
 
 def dict_factory(cursor, row):
@@ -69,14 +73,14 @@ def extract_messages(db_file):
     db = DB(db_file)
     skipped = 0
     found = 0
-    query_texts = "select * from message"
+    messages = db.query("select * from message")
+    message_list = []
 
-    for row in db.query(query_texts):
+    for row in messages:
         timestamp = row['date']
         is_imessage = False
         if not 'is_madrid' in row:
-            # New-style (?) backups
-            is_imessage = row['service'] == 'iMessage'
+            is_imessage = row['service']
             sent = row['is_sent']
         else:
             is_imessage = row['is_madrid']
@@ -93,12 +97,8 @@ def extract_messages(db_file):
         if not row['text']:
             skipped += 1
             continue
-        utc_datetime = datetime.datetime.utcfromtimestamp(timestamp)
 
         if args.sent_only and not sent:
-            skipped += 1
-            continue
-        if utc_datetime.year != args.year:
             skipped += 1
             continue
         found += 1
@@ -109,55 +109,110 @@ def extract_messages(db_file):
         else:
             address = row.get('address') or row['account']
 
-        yield dict(
-            sent='1' if sent else '0',
-            service='iMessage' if is_imessage else 'SMS',
-            subject=(row['subject'] or ''),
-            text=(row['text'] or '').replace('\n', r'\n'),
-            timestamp=timestamp,
-            address=address,
-        )
+        row_data = dict(sent='1' if sent else '0',
+                    service='iMessage' if is_imessage else 'SMS',
+                    subject=(row['subject'] or ''),
+                    text=(row['text'] or '').replace('\n', r'\n'),
+                    timestamp=timestamp,
+                    address=address,
+                    guid=row['guid']
+                    )
+        message_list.append(row_data)
 
     print('found {0} skipped {1}'.format(found, skipped))
+    return message_list
 
 
-def set_privacy(item):
+def set_privacy(db_message_list):
     """
     Hide values by default for privacy.
     """
 
     privacy_text = "Text hidden for privacy. Use -p flag to enable text."
-    item['text'] = privacy_text
+    for message_list in db_message_list:
+        for item in message_list:
+            item['text'] = privacy_text
 
 
-def write_csv(file_object):
-    fieldnames = {"timestamp": None, "service": None, "sent": None, "address": None, "subject": None, "text": None}
-    ordered_fieldnames = OrderedDict(sorted(fieldnames.items(), key=lambda t: t[0]))
+def compare_csv(file_name, db_message_list):
+    db_guid_list = []
+    file_guid_list = []
+    compared_list = []
+
+    for message_list in db_message_list:
+        for item in message_list:
+            db_guid_list.append(item['guid'])
+
+    with open(file_name, newline='') as f:
+        reader = csv.DictReader(f)
+        for item in reader:
+            file_guid_list.append(item['guid'])
+
+    compared_set = set(db_guid_list) - set(file_guid_list)
+
+    for message_list in db_message_list:
+        for item in message_list:
+            if item['guid'] in compared_set:
+                compared_list.append(item)
+
+    return compared_list
+
+
+def write_new_csv(file_object, ordered_fieldnames):
     writer = csv.DictWriter(file_object, fieldnames=ordered_fieldnames)
     writer.writeheader()
-    pattern = os.path.expanduser(args.input_pattern)
-    input_pattern_list = glob.glob(pattern)
-    for db_file in input_pattern_list:
-        print("reading {0}. use --input-pattern to select only this file".format(db_file))
-        for item in extract_messages(db_file):
-            if args.privacy:
-                set_privacy(item)
+    db_message_list = get_db_message_list()
+    set_privacy(db_message_list)
+    for message_list in db_message_list:
+        for item in message_list:
             writer.writerow(item)
 
 
+def append_csv(file_object, ordered_fieldnames, compared_list):
+    writer = csv.DictWriter(file_object, fieldnames=ordered_fieldnames)
+    for item in compared_list:
+        writer.writerow(item)
+
+
+def get_db_message_list():
+    db_message_list = []
+    pattern = os.path.expanduser(args.input_pattern)
+    input_pattern_list = glob.glob(pattern)
+    for db_file in input_pattern_list:
+        print("reading {0}.".format(db_file))
+        message_list = extract_messages(db_file)
+        db_message_list.append(message_list)
+    return db_message_list
+
+
 def run():
-    print('writing out to {0}'.format(args.output_file))
-    with open(args.output_file, 'w', encoding="utf8") as f:
-        write_csv(f)
+    fieldnames = {"timestamp": None, "service": None, "sent": None, "address": None, "subject": None, "text": None, "guid": None}
+    ordered_fieldnames = OrderedDict(sorted(fieldnames.items(), key=lambda t: t[0]))
+
+    if os.path.exists(args.output_file):
+        db_message_list = get_db_message_list()
+        set_privacy(db_message_list)
+        compared_list = compare_csv(args.output_file, db_message_list)
+        count = len(compared_list)
+        if compared_list:
+            print("{0} new messages detected. Adding messages to {1}.".format(count, args.output_file))
+            with open(args.output_file, 'a', encoding="utf8") as f:
+                append_csv(f, ordered_fieldnames, compared_list)
+        else:
+            print("Zero new messages detected. No messages added.")
+    else:
+        print('Writing messages to new file at {0}'.format(args.output_file))
+        with open(args.output_file, 'w', encoding="utf8") as f:
+            write_new_csv(f, ordered_fieldnames)
 
 
 if __name__ == "__main__":
-    output_time = time.strftime("%Y-%m-%d-%H%M%S", time.localtime())
+    backup_location = backup_location(sys.platform)
+
     parser = argparse.ArgumentParser(description="Convert iMessage texts from iPhone backup files to csv.")
-    parser.add_argument("-i", "--input_pattern", type=str, default=DEFAULT_BACKUP_LOCATION_WIN,
+    parser.add_argument("-i", "--input_pattern", type=str, default=backup_location,
                             help="The location(s) of your iPhone backup files. Will match patterns according to glob syntax.")
-    parser.add_argument("-y", "--year", type=int, default=2012, help="The year for which you want to output texts.")
-    parser.add_argument("-o", "--output_file", type=str, default=("txt_messages_{0}.csv".format(output_time)),
+    parser.add_argument("-o", "--output_file", type=str, default=("txt_messages.csv"),
                             help="The output file name.")
     parser.add_argument("-s", "--sent_only", action="store_true", default=False, help="Output only sent texts. Excludes all other texts.")
     parser.add_argument("-p", "--privacy", action="store_true", default=True, help="Enable privacy measures.")
